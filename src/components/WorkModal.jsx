f5146c85671986fd3452c854ae54a9184da3c97c
samabdullaev <abdullaevsamandar2004@gmail.com>
2024-10-03 18:10:24 +0100

    images added, format changed in work modal

--- src/components/WorkModal.jsx
+++ src/components/WorkModal.jsx
@@ -2,10 +2,37 @@ import { forwardRef, useImperativeHandle, useState } from 'react';
 import { StarsCanvas } from './canvas';
 import { close } from '../assets';
 import { urlFor } from '../client';
+import 'slick-carousel/slick/slick-theme.css';
+import 'slick-carousel/slick/slick.css';
+import Slider from 'react-slick';
+
+const settings = {
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	autoplay: true,
+	autoplaySpeed: 2000,
+	dots: false,
+	arrows: false,
+};
 
 const WorkModal = (props, ref) => {
 	const [toggle, setToggle] = useState(false);
 	const [data, setData] = useState(null);
+
+	const imageData = [
+		{
+			url: 'https://www.liquidplanner.com/wp-content/uploads/2019/04/HiRes-17.jpg',
+			alt: 'project image 1',
+		},
+		{
+			url: 'https://d31i9b8skgubvn.cloudfront.net/folder/encyclopedia/Project.jpg',
+			alt: 'project image 2',
+		},
+		{
+			url: 'https://marvel-b1-cdn.bc0a.com/f00000000100045/www.elmhurst.edu/wp-content/uploads/2018/12/5-skills-project-management-degree-elmhurst-college-infographic-thumb.jpg',
+			alt: 'project image 3',
+		},
+	];
 
 	useImperativeHandle(ref, () => ({
 		open: (data) => {
@@ -29,6 +56,7 @@ const WorkModal = (props, ref) => {
 			}}
 		>
 			<div className='bg-tertiary p-10 relative'>
+				{/* Close Button */}
 				<div className='absolute right-5 top-5'>
 					<button onClick={closeModal}>
 						<img
@@ -39,6 +67,7 @@ const WorkModal = (props, ref) => {
 					</button>
 				</div>
 
+				{/* Project Title and Description */}
 				<div>
 					<h3 className='font-medium font-base-font text-white text-[35px] leading-[130.5%] mb-[20px] md:text-[25px] '>
 						{data.title}
@@ -46,74 +75,137 @@ const WorkModal = (props, ref) => {
 					<p className='text-[16px] leading-[26px] font-normal text-[#D8D8D8] mb-[20px]'>
 						{data.description}
 					</p>
+				</div>
 
-					<div className='w-full ml-auto mb-[50px] lg:mb-[40px] shadow-[0_8px_17px_0_rgba(0,0,0,20%),0_6px_20px_0_rgba(0,0,0,19%)]'>
-						<ul className='w-full py-[10px] px-[30px] shadow-[0px_2px_10px_rgba(14,29,44,0.15)] flex justify-between gap-4'>
-							<li className='text-[16px] text-white font-semibold flex-1'>
-								Client :
-								<span className='text-[#D8D8D8] font-normal ml-2'>
-									wpOceans
-								</span>
-							</li>
-							<li className='text-[16px] text-white font-semibold flex-1'>
-								Project Type :
-								<span className='text-[#D8D8D8] font-normal ml-2'>Website</span>
-							</li>
-							<li className='text-[16px] text-white font-semibold flex-1'>
-								Completion :
-								<span className='text-[#D8D8D8] font-normal ml-2'>
-									15 Apr 2022
-								</span>
-							</li>
-							<li className='text-[16px] text-white font-semibold flex-1'>
-								Duration :
-								<span className='text-[#D8D8D8] font-normal ml-2'>
-									6 months
-								</span>
-							</li>
-						</ul>
+				{/* Project Images and Info */}
+				<div className='grid grid-cols-12 gap-4'>
+					{/* Project Images */}
+					<div className='col-span-7'>
+						{data.images?.length && (
+							<Slider {...settings}>
+								{imageData.map((imageItem, index) => (
+									<div key={index} className='h-80 w-full'>
+										<img
+											className='h-full w-full object-contain cursor-pointer transition duration-200 ease-in-out transform hover:scale-110 rounded-lg'
+											src={imageItem.url}
+											alt='Project image'
+											width={500}
+											height={320}
+										/>
+									</div>
+								))}
+								{/* {data.images.map((imageItem, index) => (
+									<div key={index} className='h-80 w-full'>
+										<img
+											className='h-full w-full object-contain cursor-pointer transition duration-200 ease-in-out transform hover:scale-110 rounded-lg'
+											src={urlFor(imageItem)}
+											alt='Project image'
+											width={500}
+											height={320}
+										/>
+									</div>
+								))} */}
+							</Slider>
+						)}
+					</div>
+					{/* Project Info */}
+					<div className='col-span-5'>
+						<div className='max-w-[380px] ml-auto flex-[35%] lg:flex-[100%] mb-[50px] lg:mb-[40px] shadow-[0_8px_17px_0_rgba(0,0,0,20%),0_6px_20px_0_rgba(0,0,0,19%)] md:ml-0 md:max-w-full md:mt-[20px]'>
+							<ul className='py-[10px] px-[30px] shadow-[0px_2px_10px_rgba(14,29,44,0.15)] md:ml-0 md:max-w-full col:py-[50px] col:px-[20px]'>
+								<li className='flex py-[10px] text-[16px] text-white font-semibold relative justify-between '>
+									Client :
+									<span className='text-[#D8D8D8] font-normal'>wpOceans</span>
+								</li>
+								<li className='flex py-[10px] text-[16px] text-white font-semibold relative justify-between '>
+									GitHub :
+									<span className='text-[#D8D8D8] font-normal'>wpOceans</span>
+								</li>
+								<li className='flex py-[10px] text-[16px] text-white font-semibold relative justify-between '>
+									Preview :
+									<span className='text-[#D8D8D8] font-normal'>wpOceans</span>
+								</li>
+								<li className='flex py-[10px] text-[16px] text-white font-semibold relative justify-between '>
+									Role :
+									<span className='text-[#D8D8D8] font-normal'>wpOceans</span>
+								</li>
+								<li className='flex py-[10px] text-[16px] text-white font-semibold relative justify-between '>
+									Start :
+									<span className='text-[#D8D8D8] font-normal'>6 Month</span>
+								</li>
+								<li className='flex py-[10px] text-[16px] text-white font-semibold relative justify-between '>
+									Completion :
+									<span className='text-[#D8D8D8] font-normal'>
+										15 Apr 2022
+									</span>
+								</li>
+							</ul>
+						</div>
 					</div>
 				</div>
 
+				{/* Project Detailed Info */}
 				<div className='gap-4 mt-[15px]'>
+					{/* Challenges */}
 					<div className=''>
 						<h3 className='font-medium font-base-font text-white text-[35px] leading-[130.5%] mb-[20px] md:text-[25px]'>
 							Challenges
 						</h3>
 						<ul>
-							{data.challenges.map((challenge, index) => (
+							{data.challenges.map((challengeItem, index) => (
 								<li
-									index={index}
+									key={index}
 									className="py-[10px] text-white pl-[35px] col:text-[14px] relative before:absolute before:left-0 before:top-1/2 before:transform before:w-[25px] before:h-[25px] before:leading-[25px] before:rounded-[50%] before:text-center before:text-[14px] before:text-[#59C378] before:bg-[#59c3781a] before:-translate-y-1/2 before:font-['themify'] before:content-['\e64c']"
 								>
-									Non saed velit dictum quam risus pharetra esta.
+									{challengeItem.challenge} → {challengeItem.solution}
 								</li>
 							))}
 						</ul>
 					</div>
+					{/* Responsibilites */}
+					<div className='md:mt-[40px]'>
+						<h3 className='font-medium font-base-font text-white text-[35px] leading-[130.5%] mb-[20px] md:text-[25px]'>
+							Responsibilities
+						</h3>
+						<ul>
+							{data.responsibilities.map((responsibilityItem, index) => (
+								<li
+									key={index}
+									className="py-[10px] text-white pl-[35px] col:text-[14px] relative before:absolute before:left-0 before:top-1/2 before:transform before:w-[25px] before:h-[25px] before:leading-[25px] before:rounded-[50%] before:text-center before:text-[14px] before:text-[#59C378] before:bg-[#59c3781a] before:-translate-y-1/2 before:font-['themify'] before:content-['\e64c']"
+								>
+									{responsibilityItem.description}
+								</li>
+							))}
+						</ul>
+					</div>
+					{/* Results */}
 					<div className='md:mt-[40px]'>
 						<h3 className='font-medium font-base-font text-white text-[35px] leading-[130.5%] mb-[20px] md:text-[25px]'>
 							Results
 						</h3>
-						<p className='text-[16px] leading-[26px] font-normal text-[#D8D8D8] mb-[20px]'>
-							Massa volutpat odio facilisis purus sit elementum. Non, sed velit
-							dictum quam. Id risus pharetra est, at rhoncus, nec ullamcorper
-							tincidunt. Id aliquet duis sollicitudin diam.
-						</p>
 						<ul>
-							<li className="py-[10px] text-white pl-[35px] col:text-[14px] relative before:absolute before:left-0 before:top-1/2 before:transform before:w-[25px] before:h-[25px] before:leading-[25px] before:rounded-[50%] before:text-center before:text-[14px] before:text-[#59C378] before:bg-[#59c3781a] before:-translate-y-1/2 before:font-['themify'] before:content-['\e64c']">
-								Mauris dignissim blandit cursus imperdiet accumsan lorem.
-							</li>
-							<li className="py-[10px] text-white pl-[35px] col:text-[14px] relative before:absolute before:left-0 before:top-1/2 before:transform before:w-[25px] before:h-[25px] before:leading-[25px] before:rounded-[50%] before:text-center before:text-[14px] before:text-[#59C378] before:bg-[#59c3781a] before:-translate-y-1/2 before:font-['themify'] before:content-['\e64c']">
-								Nam id in non sed cras purus nunc et.
-							</li>
-							<li className="py-[10px] text-white pl-[35px] col:text-[14px] relative before:absolute before:left-0 before:top-1/2 before:transform before:w-[25px] before:h-[25px] before:leading-[25px] before:rounded-[50%] before:text-center before:text-[14px] before:text-[#59C378] before:bg-[#59c3781a] before:-translate-y-1/2 before:font-['themify'] before:content-['\e64c']">
-								Mauris orci, cursus nisl odio est adipiscing gravida magna eget.
-							</li>
-							<li className="py-[10px] text-white pl-[35px] col:text-[14px] relative before:absolute before:left-0 before:top-1/2 before:transform before:w-[25px] before:h-[25px] before:leading-[25px] before:rounded-[50%] before:text-center before:text-[14px] before:text-[#59C378] before:bg-[#59c3781a] before:-translate-y-1/2 before:font-['themify'] before:content-['\e64c']">
-								Quis mauris vel felis convallis nulla dignissim.
-							</li>
+							{data.whatILearned.map((lessonItem, index) => (
+								<li
+									key={index}
+									className="py-[10px] text-white pl-[35px] col:text-[14px] relative before:absolute before:left-0 before:top-1/2 before:transform before:w-[25px] before:h-[25px] before:leading-[25px] before:rounded-[50%] before:text-center before:text-[14px] before:text-[#59C378] before:bg-[#59c3781a] before:-translate-y-1/2 before:font-['themify'] before:content-['\e64c']"
+								>
+									{lessonItem.lesson}
+								</li>
+							))}
 						</ul>
+					</div>
+					{/* Technologies */}
+					<div className='md:mt-[40px]'>
+						<h3 className='font-medium font-base-font text-white text-[35px] leading-[130.5%] mb-[20px] md:text-[25px]'>
+							Technologies
+						</h3>
+						{data.technologies.map((technologyItem, index) => (
+							<div
+								key={index}
+								className='bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary flex space-x-4'
+							>
+								{technologyItem.name}
+							</div>
+						))}
 					</div>
 				</div>
 			</div>
